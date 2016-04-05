ce558c48cc8784dc75b7e240d4f94965fa6b87bb
anupait <anooprai790@gmail.com>
2016-04-05 23:18:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<iframe width="1140" height="460" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.it/maps?q=DELHI&output=embed"></iframe>
+[3:11:59 PM] Santosh Kumar: 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
